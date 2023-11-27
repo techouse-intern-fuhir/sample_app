83ae21b85d1ad41dd6350963e96d8a3d3941e9e5
techouse-intern-fuhir <151704116+techouse-intern-fuhir@users.noreply.github.com>
2023-11-27 12:04:24 +0000

    Implement advanced login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,22 +1,53 @@
 module SessionsHelper
+
+  #渡されたユーザーでログインする
   def log_in(user)
     session[:user_id] = user.id
+    #セッションリプレス攻撃から保護する
+    #user_idに加えてもう1つミュータブルなデータポイントを追加し、両方が有効な場合にのみユーザーを認証するように
+    session[:session_token] = user.session_token
+  end
+  
+  #永続的セッションのためにユーザーをデータベースに記憶する
+  def remember(user)
+    user.remember
+    cookies.permanent.encrypted[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
   end
 
-  #loginしているユーザーがいるときだけ返す
+  # 記憶トークンcookieに対応するユーザーを返す
   def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      user = User.find_by(id: user_id)
+      # user_idに加えてもう1つミュータブルなデータポイントを追加し、両方が有効な場合にのみユーザーを認証するように
+      if user && session[:session_token] == user.remember_digest
+        @current_user = user
+      end
+    elsif (user_id = cookies.encrypted[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
     end
   end
+  
   
   #if user loged in then true, if other false, is sent
   def logged_in?
     !current_user.nil?
   end
 
+  #永続的セッションを破壊する
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
   #現在のユーザーをログアウト
   def log_out
+    forget(current_user)
     reset_session
     @current_user = nil
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  #仮想remember_tokenを作成
+  attr_accessor :remember_token
   before_save { email.downcase! }
   validates :name, presence: true,
                    length: { maximum: 50 }
@@ -21,6 +23,39 @@ class User < ApplicationRecord
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  # return a random token
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  #永続的セッションのためにユーザーをデータベースに記憶する
+  #update_attribute→dbの中身の変更＋dbと結びついたインスタンス変更
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+    remember_digest
+  end
+
+  #渡されたトークンがダイジェストと一致したらtrueを返す
+  #remember_digest→self.remember_digest(UserModel)
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    #BCrypt::Password.new(remember_digest).is_password?(remember_token)→比較演算子が再定義されている
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  #ユーザーのログイン情報を破棄する
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
+  #セッションハイジャック防止のためにセッショントークンを返す
+  #この記憶ダイジェストを再利用しているのは単に利便性のため
+  #remember_digest→self.remember_digest(UserModel)
+  def session_token
+    remember_digest || remember
   end
 
 end
